7bdb4020cff86dd0b950d1fd577ddba9749e4898
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-09-04 13:25:30 +0530

    API Code to run any Linux Command

---
+++ API.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("content-type: text/html")
+print()
+
+mydata = cgi.FieldStorage()
+command = mydata.getvalue("y")
+output = subprocess.getoutput("sudo " + command)
+
+print(output)
